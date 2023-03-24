2f9e38c3f9eb06639185d6c7c49b4054dac899ee
fernandojr999 <fpjpro@gmail.com>
2023-03-24 16:31:00 -0300

    IA reverse

---
+++ IA Reverse/index.js
@@ -0,0 +1,18 @@
+function resposta(pergunta){
+    let indice = Math.floor(Math.random() * 6);
+
+    return respostas[indice];
+}
+
+let respostas = [
+    'Eu não sei responder essa.',
+    'Tua prima',
+    'Azul',
+    '16',
+    'Ti criticam',
+    'Já entendi agora'
+]
+
+let piadas = [
+    'piada 1'
+]
